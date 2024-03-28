76495435a0e9376f7df3780b8f3dd88043b280d4
ShirYahav <shiryahav07@gmail.com>
2024-03-28 11:13:31 +0200

    first commit to changes branch

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hello")
+//Tasks:
+// Extract all countries from api using fetch and present 
+// make filter button work
+//when clicking a country switch to details.html
+
+const requestCountries = async () => {
+    const response = await fetch("https://restcountries.com/v3.1/all");
+    const countries = await response.json();
+    console.log(countries);
+}
+requestCountries();
